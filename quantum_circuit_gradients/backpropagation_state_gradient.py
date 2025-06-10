01aea97adfb2769b7e2a4586ad9ab85e0c763bbc
MartinSantesteban <martin.p.santesteban@gmail.com>
2025-06-10 12:03:25 -0300

    cambios flag de entorno

--- quantum_circuit_gradients/backpropagation_state_gradient.py
+++ quantum_circuit_gradients/backpropagation_state_gradient.py
@@ -6,7 +6,7 @@ from qiskit.circuit import QuantumCircuit
 from .split_circuit import split
 from .gradient_lookup import analytic_gradient
 from numpy.typing import NDArray
-
+import os
 
 class BackpropagationStateGradient:
     """A class to compute gradients of expectation values."""
@@ -35,48 +35,55 @@ class BackpropagationStateGradient:
         phi = Statevector.from_label("0"*ansatz.num_qubits).evolve(ansatz)
         lam = phi.evolve(op)
         e = phi.expectation_value(op)
-        grads = []
-        for j in reversed(range(num_parameters)):
-            uj = ulist[j]
+        in_training_loop = (os.environ.get("ESTADO_GLOBAL_EN_ENTRENAMIENTO", "True") == "True")
+        if in_training_loop:
+            print(" Calculo del gradiente analitico")
+            grads = []
+            for j in reversed(range(num_parameters)):
+                uj = ulist[j]
 
-            deriv = analytic_gradient(uj, paramlist[j][0])
-            for _, gate in deriv:
-                self._bind(gate, parameter_binds, inplace=True)
+                deriv = analytic_gradient(uj, paramlist[j][0])
+                for _, gate in deriv:
+                    self._bind(gate, parameter_binds, inplace=True)
 
-            uj_dagger = self._bind(uj, parameter_binds).inverse()
+                uj_dagger = self._bind(uj, parameter_binds).inverse()
 
-            phi = phi.evolve(uj_dagger)
+                phi = phi.evolve(uj_dagger)
 
-            # TODO use projection
-            grad = 2 * sum(coeff * lam.conjugate().data.dot(phi.evolve(gate).data)
-                           for coeff, gate in deriv).real
-            grads += [grad]
+                # TODO use projection
+                grad = 2 * sum(coeff * lam.conjugate().data.dot(phi.evolve(gate).data)
+                            for coeff, gate in deriv).real
+                grads += [grad]
 
-            if j > 0:
-                lam = lam.evolve(uj_dagger)
+                if j > 0:
+                    lam = lam.evolve(uj_dagger)
 
-        accumulated, unique_params = self._accumulate_product_rule(
-            list(reversed(grads)))
-
-        return e, [accumulated[unique_params.index(p)] for p in list(self.ansatz.parameters)]
+            accumulated, unique_params = self._accumulate_product_rule(
+                list(reversed(grads)))
+        ret_snd = ([accumulated[unique_params.index(p)] for p in list(self.ansatz.parameters)] if in_training_loop else None)
+        return e, ret_snd
 
     def gradients(self, parameter_binds: NDArray):
         expectation_values = []
         grads = []
-        batch_size = len(parameter_binds)
-        if batch_size <= 300:
-            map_func = map
-        else:
-            executor = concurrent.futures.ProcessPoolExecutor()
-            map_func = executor.map
-
+        #batch_size = len(parameter_binds)
+        #if batch_size <= 300:
+        #    map_func = map
+        #else:
+        #    print("multiprocesando")
+        #    executor = concurrent.futures.ProcessPoolExecutor(max_workers=5)
+        #    map_func = executor.map
+        map_func = map
         for e, grad in map_func(self.gradients_single, parameter_binds):
             expectation_values.append(e)
-            grads.append(grad)
+            if grad != None:
+                grads.append(grad)
+        if len(grads) == 0:
+            grads = None
 
-        if batch_size > 300:
-            executor.shutdown(wait=True)
-
+        #if batch_size > 300:
+        #    executor.shutdown(wait=True)
+        
         return expectation_values, grads
 
     def _accumulate_product_rule(self, gradients):
